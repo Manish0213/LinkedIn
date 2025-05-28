faf63780a779a0f34ec0f891ba91b055fa040150
manish <ratnawatmanish031@gmail.com>
2025-05-28 05:58:32 +0530

    Initial commit for LinkedIn Microservices project

---
+++ posts-service/src/main/java/com/project/linkedin/posts_service/controller/PostController.java
@@ -0,0 +1,40 @@
+package com.project.linkedin.posts_service.controller;
+
+import com.project.linkedin.posts_service.auth.UserContextHolder;
+import com.project.linkedin.posts_service.dto.PostCreateRequestDto;
+import com.project.linkedin.posts_service.dto.PostDto;
+import com.project.linkedin.posts_service.entity.Post;
+import com.project.linkedin.posts_service.service.PostsService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/core")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostsService postsService;
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostCreateRequestDto postDto) {
+        PostDto createdPost = postsService.createPost(postDto);
+        return new ResponseEntity<>(createdPost,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostDto> getPost(@PathVariable Long postId) {
+        PostDto postDto = postsService.getPostById(postId);
+        return ResponseEntity.ok(postDto);
+    }
+
+    @GetMapping("/users/{userId}/allposts")
+    public ResponseEntity<List<PostDto>> getAllPostsOfUser(@PathVariable Long userId) {
+        List<PostDto> posts = postsService.getAllPostsOfUser(userId);
+        return ResponseEntity.ok(posts);
+    }
+}
